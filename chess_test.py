b510c1625ef8340d041517a79d27b271853014d1
ritviksingh9 <ritviksingh9@users.noreply.github.com>
2020-07-03 18:03:17 -0400

    Add files via upload
    Improve efficiency reflecting board at the beginning

--- chess_test.py
+++ chess_test.py
@@ -1,5 +1,6 @@
 import chess
 from itertools import compress
+import time
 
 pawn_pos_eval_w = [0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,  0.0,
                     5.0,  5.0,  5.0,  5.0,  5.0,  5.0,  5.0,  5.0,
@@ -61,8 +62,23 @@ king_pos_eval_w = [-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0,
                         ]
 king_pos_eval_b = [king_pos_eval_w[i] for i in range(63, -1, -1)]
 
-eval_list_w = [pawn_pos_eval_w, knight_pos_eval_w, bishop_pos_eval_w, rook_pos_eval_w, queen_pos_eval_w, king_pos_eval_w]
-eval_list_b = [pawn_pos_eval_b, knight_pos_eval_b, bishop_pos_eval_b, rook_pos_eval_b, queen_pos_eval_b, king_pos_eval_b]
+eval_list_w_temp = [pawn_pos_eval_w, knight_pos_eval_w, bishop_pos_eval_w, rook_pos_eval_w, queen_pos_eval_w, king_pos_eval_w]
+eval_list_b_temp = [pawn_pos_eval_b, knight_pos_eval_b, bishop_pos_eval_b, rook_pos_eval_b, queen_pos_eval_b, king_pos_eval_b]
+
+#Reflecting the boards to account for how the bitboard is displayed
+eval_list_w = []
+eval_list_b = []
+for i in range(6):
+    board_w = eval_list_w_temp[i]
+    board_b = eval_list_b_temp[i]
+    w = []
+    b = []
+    for a in range(7, -1, -1):
+        w.extend(board_w[8*a:8*a+8])
+        b.extend(board_b[8*a:8*a+8])
+    eval_list_w.append(w)
+    eval_list_b.append(b)
+
 piece_worth = [10, 30, 30, 50, 90, 900]
 
 def eval_pos(board, side_color):
@@ -71,26 +87,18 @@ def eval_pos(board, side_color):
     #adding up pieces from white's side
     for i in range(6):
         bitboard = board.pieces(i+1, 1).tolist()
-        #bitboard.reverse()
-        w = []
-        for a in range(7, -1, -1):
-            w.extend(bitboard[8*a:8*a+8])
         #summing up the positional advantage of the material
-        score_w += sum(list(compress(eval_list_w[i], w)))
+        score_w += sum(list(compress(eval_list_w[i], bitboard)))
         #summing up the worth of the material
-        score_w += sum(w) * piece_worth[i]
+        score_w += sum(bitboard) * piece_worth[i]
 
     #adding up pieces from black's side
     for i in range(6):
         bitboard = board.pieces(i+1, 0).tolist()
-        #bitboard.reverse()
-        b = []
-        for a in range(7, -1, -1):
-            b.extend(bitboard[8*a:8*a+8])
         #summing up the positional advantage of the material
-        score_b += sum(list(compress(eval_list_b[i], b)))
+        score_b += sum(list(compress(eval_list_b[i], bitboard)))
         #summing up the worth of the material
-        score_b += sum(b) * piece_worth[i]
+        score_b += sum(bitboard) * piece_worth[i]
 
     return score_w-score_b
 
@@ -141,12 +149,16 @@ def best_move(board, side_color, depth):
 
     return best
 
-board = chess.Board()
-while 1 == 1:
-    print("Your move:")
-    x = input()
-    board.push(chess.Move.from_uci(x))
-    print(board)
-    print("Opponent move:")
-    board.push(best_move(board, 0, 4))
-    print(board)
+if __name__ == "__main__":
+    board = chess.Board()
+    while 1 == 1:
+        print("Your move:")
+        x = input()
+        board.push(chess.Move.from_uci(x))
+        print(board)
+        print("Opponent move:")
+        start = time.time()
+        board.push(best_move(board, 0, 4))
+        end = time.time()
+        print("ELAPSED TIME: ", end-start)
+        print(board)
