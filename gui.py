e7ac607f4aa4b6603098bdea0a435a876fee27f1
Hshmat <hshmatsahak@gmail.com>
2020-07-10 02:07:36 -0400

    copied pygame chess

--- gui.py
+++ gui.py
@@ -14,7 +14,7 @@ Initialize a global dictionary of images. Called once
 def loadImages():
     pieces = ['P', 'N', 'B', 'R', 'Q', 'K', 'p', 'n', 'b', 'r', 'q', 'k']
     for piece in pieces:
-        IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
+        IMAGES[piece] = p.transform.scale(p.image.load("/home/dsl7/Documents/Summer/Side Projects/Chess-AI/images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
     # we can now access images by doing, say, IMAGES['wp']
 
 def drawGameState(screen, board):
@@ -22,14 +22,18 @@ def drawGameState(screen, board):
     drawPieces(screen, board)
 
 def drawBoard(screen):
-    colors = [p.Colors("white"), p.Color("gray")]
+    colors = [p.Color("white"), p.Color("gray")]
     for r in range(DIMENSION):
         for c in range(DIMENSION):
             color = colors[(r+c)%2]
             p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
 def drawPieces(screen, board):
-    pass
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board.piece_at(chess.square(c, 7-r))
+            if piece is not None:
+                screen.blit(IMAGES[piece.symbol()], p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
 def main():
     p.init()
@@ -41,8 +45,8 @@ def main():
     while running:
         for e in p.event.get():
             if e.type == p.QUIT:
-                running = True
-        drawGameState(screen, chess.board())
+                running = False
+        drawGameState(screen, chess.Board())
         clock.tick(MAX_FPS)
         p.display.flip()
 
